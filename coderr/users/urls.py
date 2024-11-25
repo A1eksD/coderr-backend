c614be81838c2f95ba7fd34e1d5f4e5b52c75a9d
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-11-25 20:49:01 +0100

    added login function

--- coderr/users/urls.py
+++ coderr/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from users.views import RegisterUserViewSet
+from users.views import RegisterUserViewSet, LoginViewSet
 
 
 router = DefaultRouter()
 router.register(r'registration', RegisterUserViewSet, basename='registration')
+router.register(r'login', LoginViewSet, basename='login')
 
 all_name = 'users'
 
--- coderr/users/views.py
+++ coderr/users/views.py
@@ -6,7 +6,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate, login
 
 
 
@@ -25,3 +26,20 @@ class RegisterUserViewSet(viewsets.ModelViewSet):
                 'email': user.email
             },  status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class LoginViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+    
+    def create(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+            
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
