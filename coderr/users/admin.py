5b413b727f1930f0d190c97b20a90175472a9256
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-11-25 17:20:24 +0100

    register models at adminpannel

--- coderr/users/admin.py
+++ coderr/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from users.models import CustomUser, PasswordReset
 
-# Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(PasswordReset)
